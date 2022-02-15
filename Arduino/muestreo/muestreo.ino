3e6e65e3614888e3f27e1903afc5fba71a85ea02
Gustavo David Mendoza Pinto <gustavo.mendoza.dp@gmail.com>
2022-02-14 18:25:01 -0600

    Se corrigen errores para muestrear las señales, se cambia el nombre de la variable dato a indice, para hacerlo mas intuitivo, se incorporan como constantes las variables de números de datos y el tiempo de muestreo

--- Arduino/muestreo/muestreo.ino
+++ Arduino/muestreo/muestreo.ino
@@ -1,29 +1,28 @@
 
-byte intSensor = A0;    
-unsigned long int tiempo;
-unsigned long int tiempoMuestreo; 
-float datos[50];
-int dato;
-int N;
-void setup() {
-  Serial.begin(9600);
-  tiempoMuestreo = 250;
-  tiempo =  micros();
-  dato = 0;
-  N = 500;
-
+byte intSensor = A0;                            // Seleccion de sensor análogico
+unsigned long int tiempo;                       // Variable almacenadora de tiempo
+const unsigned long int tiempoMuestreo = 100;   // Variable de tiempo de muestro
+const unsigned int N = 400;                     // Número de muestras a guardar
+float datos[N];                                 // Vector de datos muestreados
+unsigned int index = 0;                         // Inicialización el indice de lectura en 0
+void setup() {        
+  Serial.begin(115200);                         // Inicializamos comunicación serial en 115200 baudios
+  tiempo =  micros();                           // Almacenamos el tiempo para esperar a guardar muestras
 }
 
 void loop() {
- while(dato < N){
-  if( micros() - tiempo > tiempoMuestreo){
-    datos[dato] = (analogRead(intSensor)/1023)*5;
-    ++dato;
+ while(index < N){                               // Inicia ciclo de llenado de datos del vector
+  if( micros() - tiempo > tiempoMuestreo){      // verificamos si ya paso el tiempo suficiente entre muestras
+    int lectura = analogRead(intSensor);        // hacemos lectura del sensor
+    float valor = (float(lectura)/1023.0)*5.0;  // convertimos el valor de lectura en un nivel de voltaje
+    datos[index] = valor;                        // guardamos el nivel de voltaje en el vector de los datos
+    ++index;                                     // incrementamos el dato 
+    tiempo = micros();                           // Almacenamos el nuevo tiempo
   }
  }
- for(int i0 = 0; i0 < N;++i0){
-  Serial.println(datos[i0]);
+ for(int i0 = 0; i0 < N;++i0){                    // Iniciamos envio de cada uno de los vectores de datos por el monitor serial
+  Serial.println(datos[i0]);                      // 
  }
- dato = 0;
+ index = 0;                                       // Reiniciamos el indice del vector de datos a 0
 
 }
